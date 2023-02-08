259efe9357a3a0884bb618f2c3250b8ee9e7d84f
dyhalmeida <dyhalmeida@gmail.com>
2023-02-08 20:26:14 -0300

    adicionado eslint
    obs 1: eslint customizado da rocketseat
    
    obs 2: necessita do plugin Eslint instalado no vscode

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello</h1>
