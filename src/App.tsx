b7b1e2c1a38e2a4b74cedf45ecbe52b6b30618de
dyhalmeida <dyhalmeida@gmail.com>
2023-02-08 20:10:58 -0300

    adicionado tema padrão no provedor de tema do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
 
   return (
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello</h1>
+    </ThemeProvider>
   )
 }
